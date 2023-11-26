49b1b55a4e1fd51387870a2f9b5d4dd9a17ab753
mar69287 <marcoa.ruiz97@gmail.com>
2023-11-26 15:15:48 -0800

    add user registration

--- src/components/AuthForms/SignUpForm.jsx
+++ src/components/AuthForms/SignUpForm.jsx
@@ -1,56 +1,72 @@
-import { Component } from 'react'
-import { signUp } from '../../utilities/users-service'
+import { useState } from 'react';
+import { useAuth } from '../../utilities/AuthContext'
 
-export default class SignUpForm extends Component {
-  state = {
-      name: '',
-      email: '',
-      password: '',
-      confirm: '',
-      error: ''
-  }
+const Signup = () => {
+  const {registerUser} = useAuth()
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [error, setError] = useState('')
 
-  handleChange = (evt) => {
-      this.setState({
-          [evt.target.name]: evt.target.value,
-          error: ''
-      })
-  }
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  handleSubmit = async (evt) => {
-      evt.preventDefault();
-      try {
-        const formData = {
-          name: this.state.name,
-          email: this.state.email,
-          password: this.state.password
-        }
-        const user = await signUp(formData)
-        this.props.setUser(user)
-      } catch {
-        this.setState({ error: 'Sign Up Failed - Try Again' })
-      }
-  } 
+    if (formData.password !== formData.confirmPassword) {
+      setError('Passwords do not match')
+      return;
+    }
 
-  render() {
-    const disable = this.state.password !== this.state.confirm;
-    return (
-      <div>
-        <div className="form-container">
-          <form autoComplete="off" onSubmit={this.handleSubmit}>
-            <label>Name</label>
-            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
-            <label>Email</label>
-            <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
-            <label>Password</label>
-            <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
-            <label>Confirm</label>
-            <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
-            <button type="submit" disabled={disable}>SIGN UP</button>
-          </form>
-        </div>
-        <p className="error-message">&nbsp;{this.state.error}</p>
-      </div>
-    );
-  }
+    try {
+      registerUser(formData)
+      setError('')
+    } catch (error) {
+      console.error('Error during signup:', error);
+      setError('Sign Up Failed - Try Again')
+    }
+  };
+  return (
+    <div>
+      {/* <h2>Signup</h2> */}
+      <form className='form-container' onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" value={formData.name} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Confirm Password:
+          <input
+            type="password"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        {error && <p>{error}</p>}
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  )
 }
+
+export default Signup
--- src/utilities/AuthContext.jsx
+++ src/utilities/AuthContext.jsx
@@ -1,11 +1,13 @@
 import { createContext, useState, useEffect, useContext } from "react";
 import { account } from '../appwriteConfig'
+import { useNavigate } from "react-router-dom";
+import { ID} from 'appwrite';
 // import * as userService from './users'
 
 const AuthContext = createContext()
 
 export const AuthProvider = ({children}) => {
-
+    const navigate = useNavigate()
     const [loading, setLoading] = useState(true)
     const [user, setUser] = useState(false)
 
@@ -34,14 +36,28 @@ export const AuthProvider = ({children}) => {
         setUser(null)
      }
 
-    const registerUser = (userInfo) => {
+     const registerUser = async (userInfo) => {
+        setLoading(true)
 
-    }
+        try{   
+            let response = await account.create(ID.unique(), userInfo.email, userInfo.password, userInfo.name);
+            await account.createEmailSession(userInfo.email, userInfo.password)
+            let accountDetails = await account.get();
+            setUser(accountDetails)
+            setLoading(false)
+            navigate('/')
+        }catch(error){
+            console.error(error)
+        }
+     }
 
     const checkUserStatus = async () => {
         try{
-            let accountDetails = await account.get();
-            setUser(accountDetails)
+            const cookieFallbackToken = localStorage.getItem('cookieFallback');
+            if (cookieFallbackToken.length>2) {
+                let accountDetails = await account.get();
+                setUser(accountDetails)
+            }
         }catch(error){
             console.error('Error during check user status:', error);
         }
