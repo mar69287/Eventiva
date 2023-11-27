61b4920ab2717bbacf23eedbdde587e18c2b750b
mar69287 <marcoa.ruiz97@gmail.com>
2023-11-26 16:45:32 -0800

    update file location

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,16 +1,13 @@
-import './App.css';
-import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import AuthPage from '../AuthPage';
-import NavBar from '../../components/NavBar/NavBar'
-import Home from '../Home';
-import PrivateRoutes from '../../utilities/PrivateRoutes';
-import { AuthProvider } from '../../utilities/AuthContext';
+import AuthPage from './AuthPage';
+import NavBar from '../components/NavBar/NavBar'
+import Home from './Home';
+import PrivateRoutes from '../utilities/PrivateRoutes';
+import { AuthProvider } from '../utilities/AuthContext';
 
 export default function App() {
 
   return (
-    <main className="App">
           <AuthProvider>
             <NavBar/>  
             <Routes>
@@ -20,8 +17,6 @@ export default function App() {
               <Route path="/auth" element={<AuthPage />} />
             </Routes>
           </AuthProvider>
-
-    </main>
   );
 }
 
