e1226bb8db46effd0320557439010ec8c359f5ec
mar69287 <marcoa.ruiz97@gmail.com>
2023-11-28 12:43:57 -0800

    user access pages

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -10,7 +10,7 @@ export default function AuthPage() {
 
     useEffect(() => {
         if (user) {
-            navigate('/')
+            navigate('/dashboard')
         }
     })
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,16 @@
+import { useNavigate } from "react-router-dom"
+import { useAuth } from "../utilities/AuthContext"
+import { useEffect } from "react"
+
 const Home = () => {
+  const navigate = useNavigate()
+  const {user} = useAuth()
+
+  useEffect(() => {
+      if (user) {
+          navigate('/dashboard')
+      }
+  })
   return (
     <div>
       <h1>Home Page</h1>
